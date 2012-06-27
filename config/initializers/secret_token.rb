637c9f30aa5c67f866c084fc0188eb181277b037
sergey_mo <svmostovoy@gmail.com>
2012-06-27 18:15:11 +0300

    init2

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hypertech::Application.config.secret_token = '61de590461e579f4192eb49e5a790a074b2602d100f4065ded119542c5cfe2df47acaa89802e2d4f4aeb6d620a84574747113757d087c3db58ad7a01c79967b4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Hypertech::Application.config.session_store :cookie_store, key: '_hypertech_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Hypertech::Application.config.session_store :active_record_store
